f0c0b59188b5da00f364c2e297386ec30e6fc293
Pritam <pritamcon02@gmail.com>
2024-07-30 01:10:31 +0530

    The script is able to read multiple HTML parsed now. User can enter any further HTML class found by adding in the text file in the specific manner as given

--- Flipkart_scrapping.py
+++ Flipkart_scrapping.py
@@ -10,19 +10,23 @@ def main():
     url = input('Enter the URL >')
     page = max_page(url)
     if not page:
-        print('Unable to proceed')
+        print('Unable to proceed. Pages not detected')
         return
     else:
         print(f'{page} total pages detected')
     
     try:
         if url.find('page=') == -1:
-            pages = int(input(f'Enter number of pages to search (maximum {page}) >'))
+            pages = int(input(f'Enter number of pages to search (between 1-{page}) >'))
+            if pages > page or pages == 0:
+                raise Exception(f'Please enter a value between 1-{page}')
         else:
             start_id = url.find('page=')+len('page=')
             start_page = int(url[start_id:start_id+1])
             print(f'You are on page {start_page}. The scrapping will start from this page')
             pages = int(input(f'Enter number of pages to search (maximum {page-start_page-1}) >'))
+            if pages > page-start_page-1 or pages == 0:
+                raise Exception(f'Please enter a value between {page}-{page-start_page-1}')
     except ValueError:
         print('Please enter a valid integer')
         return
@@ -33,17 +37,18 @@ def main():
     try:
         link_list = product_links(url,pages)
         if not link_list:
-            print('Unable to proceed')
+            print('Unable to proceed. Product links not generated')
             return
     except Exception as e:
         print(f'Unexpected error: {e}')
         print('Please check the URL or the number of pages to search')
         return
     
+
     try:
         product_details = get_details(link_list)
         if product_details is None:
-            print('Unable to proceed')
+            print('Unable to proceed. Product details not obtained')
             return
     except Exception as e:
         print(f'Unexpected error: {e}')
--- scrapping_module.py
+++ scrapping_module.py
@@ -3,7 +3,6 @@ import requests
 import os
 import platform
 from plyer import notification
-import threading
 import time
 
 # assigning headers to get proper response from the server
@@ -26,17 +25,53 @@ def clear_screen():
 
 def max_page(url):
     '''
-    Returns the total number of pages available for searching
+    Returns the total number of pages available for searching.
+    If total page is not there in the webpage, returns a value of 1
     '''
     url = url
     r = requests.get(url,headers=headers)
-    if r.status_code == 200: # checking for proper connection
-        soup = bs(r.content,'lxml')
-        pages = soup.find('div', class_ = '_1G0WLw').span.text.split(' ')[-1] # extracting the total number of pages
-        return int(pages)
-    else:
-        print('Server not responding')
+    try:
+        if r.status_code == 200: # checking for proper connection
+            soup = bs(r.content,'lxml')
+            pages = soup.find('div', class_ = '_1G0WLw').span.text.split(' ')[-1].replace(',','') # extracting the total number of pages
+            return int(pages)
+        else:
+            print('Server not responding')
+            return
+    except ValueError as v:
+        print(f'Error {v}')
         return
+    except AttributeError:
+        pages = 1
+        return pages
+
+
+def check_product_class(parsed_HTML):
+    '''
+    Take the class.txt file and creates a dictionary with number as keys,
+    product class and product link class as values in a list format.
+    Check the input parsed HTML for which product class is present and returns
+    the product link class along with the product class find all values.    
+    '''
+    class_dict = {}
+    try:
+        with open ('class.txt','r') as f:
+            for line in f:
+                g = line.strip().split(',')
+                if g[2].lower() == "none":
+                    g[2] = None
+                class_dict[int(g[0])] = [g[1],g[2]]
+    except:
+        print('Error. Check your class.txt file or directory')
+        return
+
+    for i in range(len(class_dict)):
+        product_class = class_dict[i][0]
+        product = parsed_HTML.find_all('div',class_ = product_class)
+        if product: # check here
+            break
+    product_link_class = class_dict[i][1]
+    return product, product_link_class
 
 
 def product_links(url,page=1):
@@ -46,48 +81,47 @@ def product_links(url,page=1):
     base_url='https://www.flipkart.com'
     url = url
     link_list = []
+    class_detected = False #class is not detected
     for i in range(1,page+1):
         r = requests.get(url,headers=headers)
+        
         if r.status_code == 200:
             r = r.content
             soup = bs(r,'lxml')
-            product = soup.find_all('div',class_ = 'slAVV4') # product list in page design
-            if not product:
-                product = soup.find_all('div',class_ = 'tUxRFH') # product list in another page design
+            
+            if not class_detected:
+                product, link_class = check_product_class(soup)
+                class_detected = True #class is detected
             for prod in product:
-                link = prod.find('a', class_ = 'VJA3rP') # product details in first page design
-                if not link:
-                    link = prod.find('a', class_ = 'CGtC98')['href'] # product details in second page design(extracting the link directly)
-                else:
-                    link = link['href'] # product link in first page design
+                link = prod.find('a', class_ = link_class)['href'] # product links in the page
                 link_list.append(base_url+link) # collecting the clickable link by joining the base link
-            next_link = soup.find('a',class_ = '_9QVEpD')['href'] # link to next page
-            url = base_url+next_link # clickable link of next page
+            
+            next_link = soup.find('a',class_ = '_9QVEpD')
+            if next_link is not None: #check for the presence of next link (for cases when only one page is present)
+                next_link = soup.find('a',class_ = '_9QVEpD')['href'] # link to next page
+                url = base_url+next_link # clickable link of next page
+        
         else:
             print('Request generation failed. Not able to generate links')
             print(r)
             return 
+    
     return link_list
 
 
 def get_details(link_list):
     '''
     Takes a list of clickable links and finds the Name, Rating,
-    Number of ratings, Number of reviews, Actual price, Discount price and Link of the products
+    Number of ratings, Number of reviews, Actual price, Current price and Link of the products
     to create a dictionary.
     '''
     total = len(link_list)
     print(f'Getting details for {total} products')
-    product_details = {'Name':[],"Rating":[],"Number of ratings":[],'Number of reviews':[],"Actual Price":[], "Discount Price":[], "Link":[]}
+    product_details = {'Name':[],"Rating":[],"Number of ratings":[],'Number of reviews':[],"Actual Price":[], "Current Price":[], "Link":[]}
     for i,link in enumerate(link_list,1):
         url = link
         r = requests.get(url,headers=headers)
         if r.status_code == 200:
-
-            clear_screen()
-            print(f'Getting details for {total} products')
-            print('[','█'*i,' '*(total-i),']',end=' ')
-            print(f'{i}/{total}')
 
             r = r.content
             soup = bs(r,'lxml')
@@ -126,17 +160,22 @@ def get_details(link_list):
             
             # extracting discount price
             try:
-                disprice = float(soup.find('div',class_ = 'Nx9bqj CxhGGd').text[1:].replace(',',''))
+                price = float(soup.find('div',class_ = 'Nx9bqj CxhGGd').text[1:].replace(',',''))
             except:
-                disprice = 'Not available'
+                price = 'Not available'
             
             product_details['Name'].append(name)
             product_details["Rating"].append(rating)
             product_details["Number of ratings"].append(ratings)
             product_details['Number of reviews'].append(reviews)
             product_details["Actual Price"].append(actualprice)
-            product_details["Discount Price"].append(disprice)
+            product_details["Current Price"].append(price)
             product_details['Link'] = link
+
+            clear_screen()
+            print(f'Getting details for {total} products')
+            print('[','█'*i,' '*(total-i),']',end=' ')
+            print(f'{i}/{total}')
             
         else:
             print(f'{i} not done')
